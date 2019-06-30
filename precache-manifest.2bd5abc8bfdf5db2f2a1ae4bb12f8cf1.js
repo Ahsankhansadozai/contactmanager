df6cf85254bb5335c8bd80b5e774439a24604cb2
unknown <ahsan3200@iiu.edu.pk>
2019-06-29 23:42:48 -0700

    Updates

---
+++ precache-manifest.2bd5abc8bfdf5db2f2a1ae4bb12f8cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a03fe3346dbeffcfd63212713d5eb86",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "10c7d28fe152b306a971",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b8f58ac8c319939b85bd",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "10c7d28fe152b306a971",
+    "url": "/contactmanager/static/js/2.f3e6ee30.chunk.js"
+  },
+  {
+    "revision": "b8f58ac8c319939b85bd",
+    "url": "/contactmanager/static/js/main.edecfd41.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
